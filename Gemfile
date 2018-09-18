35834258d8c42474e2986228365cd8b4e92d03e8
Nando179439 <fernando_5341@hotmail.com>
2018-09-18 14:31:31 +0000

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -15,10 +15,10 @@ gem 'jquery-rails', '4.3.1'
 gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.7.0'
 gem 'bcrypt',         '3.1.12'
+gem 'byebug',  '9.0.6', platform: :mri
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
-  gem 'byebug',  '9.0.6', platform: :mri
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.first
+user.update_attributes(name: "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar")
